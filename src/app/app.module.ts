c1ae864432d20d0a24f540d01f4974844401037f
logonrm <logonrm@fiap.com.br>
2018-03-13 22:42:18 -0300

    Aula de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,31 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'; 
+import { FormsModule } from '@angular/forms';
+
+import { appRoutes } from './rotas/app.routes'; //deve vir primeiro
 
 import { AppComponent }  from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SubLista } from './filters/sublista.filter'; 
+
+//usado nas rotas
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { NotFoundComponent } from './erro/notFound.component';
+import { EventosService } from './services/eventos.service'; 
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, 
+    RouterModule.forRoot(appRoutes), 
+    FormsModule],
+  declarations: [ AppComponent, 
+    MenuComponent,
+    HomeComponent,
+    CadastroComponent,
+    NotFoundComponent,
+    SubLista ],
+  providers : [ EventosService ], 
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
